617466081b5170bb4bcc98936f28589b052cc8d4
Arjan van de Ven <arjanvandeven@gmail.com>
2026-05-21 14:30:34 -0700

    feat(agent): suggest message_agent in agent_get_output return
    - Updated the return string of `agent_get_output` to also suggest using `message_agent(<id>, message)` as an alternative to `end_agent`, giving the LLM clear context on how to iteratively prompt its subagents.

--- src/tools/agent_get_output/agent_get_output_entry.cpp
+++ src/tools/agent_get_output/agent_get_output_entry.cpp
@@ -41,6 +41,11 @@ std::string agent_get_output_tool::execute(agentlib::tool_context& ctx) {
         oss << interaction->get_raw_text() << "\n\n";
     }
 
+    oss << "--- End of History ---\n";
+    oss << "If you are finished with this subagent, you should use the end_agent(" << target_agent->get_id() 
+        << ") tool to clean it up and free resources. Alternatively, you can send it new instructions using the message_agent(" 
+        << target_agent->get_id() << ", \"<message>\") tool.";
+
     return oss.str();
 }
 
