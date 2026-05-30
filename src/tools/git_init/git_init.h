5263067e30e048595be6a002da2dbb3c0c472ebe
Arjan van de Ven <arjanvandeven@gmail.com>
2026-05-30 12:55:25 -0700

    Refactor git_init to use zero_argument_tool_validator

--- src/tools/git_init/git_init.h
+++ src/tools/git_init/git_init.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 #include "../../agentlib/llm_tool_action.h"
-#include "../../agentlib/tool_validator.h"
+#include "../../agentlib/zero_argument_tool_validator.h"
 
 namespace tools {
 
@@ -13,26 +13,15 @@ public:
     std::string execute(agentlib::tool_context& ctx) override;
 };
 
-class git_init_validator : public agentlib::tool_validator {
+class git_init_validator : public agentlib::zero_argument_tool_validator {
 public:
     std::string get_name() const override { return "git_init"; }
     std::string get_description() const override { return "Initialize a new Git repository in the current project root. Fails if a .git directory already exists."; }
     
-    nlohmann::json get_parameters_schema() const override {
-        return {
-            {"type", "object"},
-            {"properties", nlohmann::json::object()}
-        };
-    }
-    
     bool is_pure() const override { return false; }
 
 protected:
-    bool validate_args_impl(const nlohmann::json& /*args*/, const agentlib::tool_context& /*ctx*/, std::string& /*out_error*/) const override {
-        return true;
-    }
-
-    std::unique_ptr<agentlib::llm_tool> create_tool_impl(const nlohmann::json& /*args*/) const override {
+    std::unique_ptr<agentlib::llm_tool> create_tool_from_zero_args() const override {
         return std::make_unique<git_init_tool>();
     }
 };
